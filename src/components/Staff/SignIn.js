f1382b49dcfe049321debadc978ba029733dba4f
santigeek <santigeek@hotmail.com>
2021-04-11 23:35:15 -0500

    Fixing Login component

--- src/components/Staff/SignIn.js
+++ src/components/Staff/SignIn.js
@@ -3,33 +3,33 @@ import LogoApe from "../../assets/static/logo-ape-regular.png";
 
 const SignIn = () => {
   return (
-    <div className="bg-gray-300 h-screen py-40">
-      <div className="bg-white text-center w-80 h-80 p-2 mx-auto font-sena">
+    <div className="bg-gray-300 h-screen py-32 px-4">
+      <div className="bg-white text-center w-84 h-96 p-2 mx-auto font-sena">
         <article className="">
           <img className="w-52 p-2" src={LogoApe} alt="" />
         </article>
         <form action="">
-          <h1 className="text-xl text-principal-300">Funcionarios</h1>
+          <h1 className="mt-4 text-2xl text-principal-300">Funcionarios</h1>
           <label htmlFor="">
-            <span className="block text-left ml-16 mt-2 text-principal-100">
+            <span className="block text-left ml-16 mt-3 text-principal-100">
               Usuario:
             </span>
             <input
-              className="bg-gray-300 w-44 text-base px-1 rounded-md"
+              className="bg-gray-300 w-52 text-base px-1 rounded-md"
               type="email"
             />
           </label>
           <label className="" htmlFor="">
-            <span className="block text-left ml-16 mt-3 text-principal-100">
+            <span className="block text-left ml-16 mt-4 text-principal-100">
               Contraseña
             </span>
             <input
-              className="bg-gray-300 w-44 text-base px-1 rounded-md"
+              className="bg-gray-300 w-52 text-base px-1 rounded-md"
               type="email"
             />
           </label>
           <button
-            className="block rounded-md h-7 hover:bg-blue-450 mt-7 ml-48 bg-principal-100 text-white py-1 w-24"
+            className="block rounded-md h-7 hover:bg-blue-450 mt-12 ml-48 bg-principal-100 text-white py-1 w-24"
             placeholder="Send"
           >
             Ingresar
